a57533be762c3aed1f49db916413d214d66ea0ff
Mateusz <wirus006@gmail.com>
2021-10-05 20:15:45 +0200

    Updates

--- mail_handler.php
+++ mail_handler.php
@@ -17,15 +17,15 @@
             $message = "Od:" . $from . " Imię: " . $first_name . " telefon: " . $telephone . " Wiadomość: " . "\n\n" . $_POST['message'];
             $message2 = "Dziękujemy za wiadomość, odezwiemy się niebawem. Kopia wiadomości: " . $first_name . "\n\n" . $_POST['message'];
 
-            $headers = "Content-Transfer-Encodin: 8bitr\n"
-            $headers .= "Content-type: text/html; charset=utf-8r\n";
+            $headers = "Content-type: text/html; charset=utf-8r\n";
+            $headers .= "Content-Transfer-Encodin: 8bitr\n";
 
             $headers2 = $headers . "From:" . $to;
             $headers .= "From:" . $from;
 
 
-            imap_mail($to,$subject,$message,$headers);
-            imap_mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+            mail($to,$subject,$message,$headers);
+            mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
             echo "Mail wysłany. Dziękujemy " . $first_name . ", odezwiemy się niebawem.";
             // You can also use header('Location: thank_you.php'); to redirect to another page.
             // You cannot use header and echo together. It's one or the other.
