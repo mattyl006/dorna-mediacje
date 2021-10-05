9ea2e1c0fb3a6771835e14d5754ccb8479b9c3bd
Mateusz <wirus006@gmail.com>
2021-10-05 19:59:26 +0200

    Updates

--- mail_handler.php
+++ mail_handler.php
@@ -19,8 +19,8 @@
             $headers2 = "From:" . $to;
 
 
-            mail($to,$subject,$message,$headers);
-            mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+            imap_mail($to,$subject,$message,$headers);
+            imap_mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
             echo "Mail wysłany. Dziękujemy " . $first_name . ", odezwiemy się niebawem.";
             // You can also use header('Location: thank_you.php'); to redirect to another page.
             // You cannot use header and echo together. It's one or the other.
