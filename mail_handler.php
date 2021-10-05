40500652792d0872cf7dc384ef97bb356cfcb96e
Mateusz <wirus006@gmail.com>
2021-10-05 13:58:12 +0200

    Updates

--- mail_handler.php
+++ mail_handler.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail wysłany. Dziękujemy " . $first_name . ", odezwiemy się niebawem.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
