bd5f50314d0b13226941d7dad9dfa71ad0bf041c
Mateusz <wirus006@gmail.com>
2021-10-05 14:19:32 +0200

    correct form submit input

--- public/mail_handler.php
+++ public/mail_handler.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['submit'])){
+if(isset($_POST['submit'])) {
     $to = "joanna.dorna@mediacje.pila.pl"; // this is your Email address
     $from = $_POST['mail']; // this is the sender's Email address
     $first_name = $_POST['name'];
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail wysłany. Dziękujemy " . $first_name . ", odezwiemy się niebawem.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
