32e875b083c9628bf7a6c0f718b16d9d13101c2a
Mateusz <wirus006@gmail.com>
2021-10-06 10:10:07 +0200

    add validation to form and php script, correct one br

--- public/mail_handler.php
+++ public/mail_handler.php
@@ -5,27 +5,35 @@
         <title>Mediacje Piła - status wiadomości</title>
     </head>
     <body>
-        <?php
-        if(isset($_POST['submit'])) {
-            $to = "joanna.dorna@mediacje.pila.pl"; // this is your Email address
-            $from = $_POST['mail']; // this is the sender's Email address
-            $first_name = $_POST['name'];
-            $telephone = $_POST['telephone'];
-            $subject = "Wiadomość z formularza";
-            $subject2 = "Kopia wiadomości z formularza";
-            $message = "Od:" . $from . " Imię: " . $first_name . " telefon: " . $telephone . " Wiadomość: " . "\n\n" . $_POST['message'];
-            $message2 = "Dziękujemy za wiadomość, odezwiemy się niebawem. Kopia wiadomości: " . $first_name . "\n\n" . $_POST['message'];
-            $headers = "From:" . $from;
-            $headers2 = "From:" . $to;
+        <main>
+            <?php
+                if(isset($_POST['submit'])) {
+                    $to = "joanna.dorna@mediacje.pila.pl";
+                    $from = $_POST['mail'];
+                    $first_name = $_POST['name'];
+                    $telephone = $_POST['telephone'];
+                    $subject = "Kontakt z formularza";
+                    $subject2 = "Kontakt z formularza mediacje.pila.pl";
 
+                    $message = "Od: " . $from . "\nImię: " . $first_name . "\ntelefon: " . $telephone . "\nWiadomość: " . "\n\n" . $_POST['message'];
+                    $message2 = "Dziękuję za kontakt, odezwę się niebawem. \nKopia wiadomości: " . $first_name . "\n\n" . $_POST['message'];
 
-            mail($to,$subject,$message,$headers);
-            mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-            echo "Mail wysłany. Dziękujemy " . $first_name . ", odezwiemy się niebawem.";
-            // You can also use header('Location: thank_you.php'); to redirect to another page.
-            // You cannot use header and echo together. It's one or the other.
-            }
-        ?>
+                    $headers = "Content-type: text/html; charset=utf-8r\n";
+                    $headers .= "Content-Transfer-Encodin: 8bitr\n";
+
+                    $headers2 = $headers . "From:" . $to;
+                    $headers .= "From:" . $from;
+
+                    if(mail($to,$subject,$message,$headers) and mail($from,$subject2,$message2,$headers2)) {
+                        echo "<h1 class="success">Sukces</h1>"
+                        echo "<p>Wiadomość wysłana pomyślnie. Dziękuję " . $first_name . ", odezwę się niebawem.</p>";
+                    } else {
+                        echo "<h1 class="failure">Coś poszło nie tak</h1>"
+                        echo "<p>Wiadomość nie została wysłana, proszę spróbować jeszcze raz</p>"
+                    }
+                }
+            ?>
+        </main>
     </body>
 </html>
 
