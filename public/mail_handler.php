13cd2494e84ee6b01c05be270357b181b940f06c
Mateusz <wirus006@gmail.com>
2021-10-06 10:50:25 +0200

    correct format mails from php script and syntax errors

--- public/mail_handler.php
+++ public/mail_handler.php
@@ -57,8 +57,8 @@
                     $subject = "Kontakt z formularza";
                     $subject2 = "Kontakt z formularza mediacje.pila.pl";
 
-                    $message = "Od: " . $from . "\nImię: " . $first_name . "\ntelefon: " . $telephone . "\nWiadomość: " . "\n\n" . $_POST['message'];
-                    $message2 = "Dziękuję za kontakt, odezwę się niebawem. \nKopia wiadomości: " . $first_name . "\n\n" . $_POST['message'];
+                    $message = "Od: " . $from . "<br>Imię: " . $first_name . "<br>Telefon: " . $telephone . "<br><br>Wiadomość: " . "<br><br>" . $_POST['message'];
+                    $message2 = "Dziękuję za kontakt, odezwę się niebawem. <br><br>Kopia wiadomości: " . $first_name . "<br><br>" . $_POST['message'];
 
                     $headers = "Content-type: text/html; charset=utf-8r\n";
                     $headers .= "Content-Transfer-Encodin: 8bitr\n";
@@ -67,10 +67,10 @@
                     $headers .= "From:" . $from;
 
                     if(mail($to,$subject,$message,$headers) and mail($from,$subject2,$message2,$headers2)) {
-                        echo "<h1 class="success">Sukces</h1>";
+                        echo "<h1 class='success'>Sukces</h1>";
                         echo "<p>Wiadomość wysłana pomyślnie. Dziękuję " . $first_name . ", odezwę się niebawem.</p>";
                     } else {
-                        echo "<h1 class="failure">Coś poszło nie tak</h1>";
+                        echo "<h1 class='failure'>Coś poszło nie tak</h1>";
                         echo "<p>Wiadomość nie została wysłana, proszę spróbować jeszcze raz</p>";
                     }
                 }
